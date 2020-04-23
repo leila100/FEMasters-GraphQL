6b80f1be044b7685176c1b00a7d8e8d907ba425f
Leila <nb.leila10@gmail.com>
2020-04-22 19:52:37 -0500

    Add basic schema types

--- api/src/schema.js
+++ api/src/schema.js
@@ -1,11 +1,24 @@
-const { gql } = require('apollo-server')
+const { gql } = require("apollo-server");
 
 /**
  * Type Definitions for our Schema using the SDL.
  */
 const typeDefs = gql`
-  
+  type User {
+    id: ID!
+    userName: String!
+  }
 
+  type Pet {
+    id: ID!
+    createdAt: String!
+    name: String!
+    type: String!
+  }
+
+  type Query {
+    Pets: [Pet]!
+  }
 `;
 
-module.exports = typeDefs
+module.exports = typeDefs;
