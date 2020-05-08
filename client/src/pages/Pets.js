ac442b9389276c777ced5657950b9a1755053a81
Leila <nb.leila10@gmail.com>
2020-05-07 22:15:49 -0500

    Add query to get all pets

--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -1,53 +1,70 @@
-import React, {useState} from 'react'
-import gql from 'graphql-tag'
-import PetBox from '../components/PetBox'
-import NewPet from '../components/NewPet'
-import { useQuery, useMutation } from '@apollo/react-hooks'
-import Loader from '../components/Loader'
+import React, { useState, useEffect } from "react";
+import gql from "graphql-tag";
+import PetBox from "../components/PetBox";
+import NewPet from "../components/NewPet";
+import { useQuery, useMutation } from "@apollo/react-hooks";
+import Loader from "../components/Loader";
 
-export default function Pets () {
-  const [modal, setModal] = useState(false)
-  
-  const onSubmit = input => {
-    setModal(false)
+const GET_PETS = gql`
+  query GetPetsQuery {
+    pets {
+      id
+      createdAt
+      name
+      type
+      img
+      user {
+        id
+        userName
+      }
+    }
   }
+`;
 
-  const petsList = pets.data.pets.map(pet => (
-    <div className="col-xs-12 col-md-4 col" key={pet.id}>
-      <div className="box">
+export default function Pets() {
+  const [modal, setModal] = useState(false);
+  const { data, loading, error } = useQuery(GET_PETS);
+  if (loading) return <Loader />;
+  if (error) return `Error! ${error.message}`;
+
+  const onSubmit = (input) => {
+    setModal(false);
+  };
+
+  const petsList = data.pets.map((pet) => (
+    <div className='col-xs-12 col-md-4 col' key={pet.id}>
+      <div className='box'>
         <PetBox pet={pet} />
       </div>
     </div>
-  ))
-  
+  ));
+
   if (modal) {
     return (
-      <div className="row center-xs">
-        <div className="col-xs-8">
-          <NewPet onSubmit={onSubmit} onCancel={() => setModal(false)}/>
+      <div className='row center-xs'>
+        <div className='col-xs-8'>
+          <NewPet onSubmit={onSubmit} onCancel={() => setModal(false)} />
         </div>
       </div>
-    )
+    );
   }
 
   return (
-    <div className="page pets-page">
+    <div className='page pets-page'>
       <section>
-        <div className="row betwee-xs middle-xs">
-          <div className="col-xs-10">
+        <div className='row betwee-xs middle-xs'>
+          <div className='col-xs-10'>
             <h1>Pets</h1>
           </div>
 
-          <div className="col-xs-2">
+          <div className='col-xs-2'>
             <button onClick={() => setModal(true)}>new pet</button>
           </div>
         </div>
       </section>
       <section>
-        <div className="row">
-          {petsList}
-        </div>
+        <div className='row'>{petsList}</div>
       </section>
     </div>
-  )
+  );
 }
